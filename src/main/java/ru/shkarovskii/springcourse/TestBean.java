ad7633bc67cbc127a3525eaea78bcc91b5be03e1
shkarovskii <s9648785578@gmail.com>
2022-03-30 09:45:32 +0400

    applicationContext.mxl TestSpring.java .gitignore

--- src/main/java/ru/shkarovskii/springcourse/TestBean.java
+++ src/main/java/ru/shkarovskii/springcourse/TestBean.java
@@ -3,5 +3,15 @@ package ru.shkarovskii.springcourse;
 public class TestBean {
 	private String name;
 	
-
+	public TestBean(String name) {
+		this.name = name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
